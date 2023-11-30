37fd78420ac28ba7e4c025cd0c5b2acc9940f217
joxajudo <jalilovjahongir99@gmail.com>
2023-11-30 17:59:48 +0500

    Permission

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from app.models import Item
@@ -7,5 +8,6 @@ from app.serializers import ItemModelSerializer
 class CreateItemAPIView(ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemModelSerializer
+    permission_classes = [IsAuthenticated]
 
 
